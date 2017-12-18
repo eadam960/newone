491dbf8c17f0fdde5fba3b7edecc77e73c2abfb6
eadam <eadam@EADAM>
2017-12-18 16:19:52 +0100

    try...

---
+++ src/pb/Third.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pb;
+
+/**
+ *
+ * @author eadam
+ */
+public class Third {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    private class Fifth{
+
+    }
+    
+    public class Sixth{
+    
+    }
+    
+}
+ class Fourth{
+ 
+ }
